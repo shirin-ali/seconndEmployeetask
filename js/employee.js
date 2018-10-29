200f04df4fc673121bac827c00750e44a40a8ba2
shirin ali <shirin.ali@gspann.com>
2018-10-29 14:02:41 +0530

    changes commit

--- js/employee.js
+++ js/employee.js
@@ -201,7 +201,8 @@ var  updateView = function(arrayData){
    var  container_div = document.getElementById("container");
    container_div.classList.remove("form-hide");
    container_div.innerHTML = '';
-for(var index=0;index<arrayData.length;index++)
+   if(arrayData)
+{for(var index=0;index<arrayData.length;index++)
  {
  var cardDiv = document.createElement("div");
  cardDiv.className ="class-card";
@@ -345,7 +346,7 @@ deletelogo.onclick = function(){deleteDetail(event)};
  pathOfimage = "image/default-usertwo.jpg"
 
 
-}
+}}
 console.log(employeeData);
 }
 var  deleteDetail = function(event){
